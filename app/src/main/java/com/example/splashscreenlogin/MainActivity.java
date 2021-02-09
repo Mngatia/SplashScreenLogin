719b91618f6ebd30d170b2b097a7e9b8efa988b4
Mngatia <nyawirahngatia@gmail.com>
2021-02-09 22:50:08 +0300

    Splash Screen with Animation.

--- app/src/main/java/com/example/splashscreenlogin/MainActivity.java
+++ app/src/main/java/com/example/splashscreenlogin/MainActivity.java
@@ -3,12 +3,33 @@ package com.example.splashscreenlogin;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    //2. Widgets
+    Button btn;
+    TextView t1, t2;
+    Animation animate_btn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btn = findViewById(R.id.button);
+        t1 = findViewById(R.id.textView);
+        t2 = findViewById(R.id.textView2);
+
+        //1. Add animation
+        animate_btn = AnimationUtils.loadAnimation(this, R.anim.animate_btn);
+
+        btn.setAnimation(animate_btn);
+
+        //Animation for textview
+
     }
 }
