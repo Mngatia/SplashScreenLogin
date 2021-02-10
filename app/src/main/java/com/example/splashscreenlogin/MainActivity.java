ec6e57811417447703313ae8924f0a533911d683
Mngatia <nyawirahngatia@gmail.com>
2021-02-10 15:25:54 +0300

    Splash Screen Login Page.

--- app/src/main/java/com/example/splashscreenlogin/MainActivity.java
+++ app/src/main/java/com/example/splashscreenlogin/MainActivity.java
@@ -13,7 +13,7 @@ public class MainActivity extends AppCompatActivity {
     //2. Widgets
     Button btn;
     TextView t1, t2;
-    Animation animate_btn;
+    Animation animate_btn, animate_txt;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,11 +25,19 @@ public class MainActivity extends AppCompatActivity {
         t2 = findViewById(R.id.textView2);
 
         //1. Add animation
-        animate_btn = AnimationUtils.loadAnimation(this, R.anim.animate_btn);
+        animate_btn = AnimationUtils.loadAnimation(this,
+                R.anim.animate_btn);
+
+        animate_txt = AnimationUtils.loadAnimation(this,
+                R.anim.animate_texts);
 
         btn.setAnimation(animate_btn);
 
         //Animation for textview
+        t1.setAnimation(animate_txt);
+        t2.setAnimation(animate_txt);
+
+
 
     }
 }
